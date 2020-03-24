310b875de3719e20289c2d88cc0962eb9d461445
laurenkogelmann <lauren.kogelmann@galatea-associates.com>
2020-03-24 14:33:03 -0400

    Add AppConfig

--- src/main/java/org/galatea/starter/AppConfig.java
+++ src/main/java/org/galatea/starter/AppConfig.java
@@ -1,4 +1,39 @@
-package org.galatea.starter.entrypoint;
+package org.galatea.starter;
 
+import feign.Logger;
+import lombok.extern.slf4j.Slf4j;
+import net.sf.aspect4log.aspect.LogAspect;
+import org.galatea.starter.service.StockPriceClient;
+import org.galatea.starter.service.StockPriceService;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+
+@Slf4j
+@Configuration
+@EnableAspectJAutoProxy
+@EnableCaching
+@EnableFeignClients
 public class AppConfig {
+
+  /**
+   * Create a LogAspect for use with the SpringAOP @Log annotation.
+   */
+  @Bean
+  public LogAspect createLogAspect() {
+    return new LogAspect();
+  }
+
+  /**
+   * Set the Feign log level for interfaces annotated with @FeignClient.
+   *
+   * @return the Feign log level.
+   */
+  @Bean
+  public Logger.Level logLevel() {
+    return Logger.Level.BASIC;
+  }
+
 }
